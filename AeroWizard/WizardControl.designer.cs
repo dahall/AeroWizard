2d58d21663365cb46450da14697bd3d3faa19783
dahall_cp <SND\dahall_cp>
2016-01-04 03:10:49 +0000

    * Version 2.1.2 * Fixed minor designer problem with Next button

--- AeroWizard/WizardControl.designer.cs
+++ AeroWizard/WizardControl.designer.cs
@@ -154,7 +154,7 @@
 			this.nextButton.Name = "nextButton";
 			this.nextButton.Size = new System.Drawing.Size(70, 23);
 			this.nextButton.TabIndex = 0;
-			this.nextButton.Text = "&Next >";
+			this.nextButton.Text = "&Next";
 			this.nextButton.UseVisualStyleBackColor = true;
 			// 
 			// header

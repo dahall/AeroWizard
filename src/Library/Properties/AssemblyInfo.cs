fc6dfc50d5f0dc0c74c7035bd8ee2e69fc952e35
David Hall <dahall@byu.net>
2019-01-09 12:50:25 -0700

    Updated project versions to 2.2.2

--- src/Library/Properties/AssemblyInfo.cs
+++ src/Library/Properties/AssemblyInfo.cs
@@ -27,6 +27,6 @@ using System.Runtime.InteropServices;
 // Major Version Minor Version Build Number Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.1")]
-[assembly: AssemblyFileVersion("2.2.1")]
+[assembly: AssemblyVersion("2.2.2")]
+[assembly: AssemblyFileVersion("2.2.2")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]

5733680aedbb38128e700f303f03fc40d34f8253
dahall <dahall@byu.net>
2020-10-13 11:11:04 -0600

    Updated copyright date

--- src/Library/Properties/AssemblyInfo.cs
+++ src/Library/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GitHub Community")]
 [assembly: AssemblyProduct("AeroWizard")]
-[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
